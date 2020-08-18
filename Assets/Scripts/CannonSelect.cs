928509d32a0ba1cd28d283a7f384e478451f807b
mark-sawyer <alfador_321@hotmail.com>
2020-08-18 18:22:24 +1000

    Cannon can be clicked and aim to mouse

---
+++ Assets/Scripts/CannonSelect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonSelect : MonoBehaviour {
+    private bool isBeingHeld;
+
+    private void Update() {
+        if (isBeingHeld) {
+            if (!Input.GetMouseButton(0)) {
+                isBeingHeld = false;
+            }
+            else {
+                Vector2 mouseVector = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+                transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(mouseVector.y, mouseVector.x) * Mathf.Rad2Deg);
+            }
+        }
+    }
+
+    public void startBeingHeld() {
+        isBeingHeld = true;
+    }
+}
---
+++ Assets/Scripts/ClickManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickManager : MonoBehaviour {
+    [SerializeField] private LayerMask cannonLayer;
+
+    private void Start() {
+        cannonLayer = LayerMask.GetMask("cannon");
+    }
+
+    void Update() {
+        if (Input.GetMouseButton(0)) {
+            RaycastHit2D ray = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 0, cannonLayer);
+            if (ray.collider != null) {
+                ray.collider.GetComponent<CannonSelect>().startBeingHeld();
+            }
+        }
+    }
+}
