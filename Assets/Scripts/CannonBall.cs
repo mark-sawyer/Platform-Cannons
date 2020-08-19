117435eeefa34186f4fa10b4ab0bae2546fae74e
mark-sawyer <alfador_321@hotmail.com>
2020-08-19 17:39:53 +1000

    Cannon balls shoot ahead of the cannons and have velocity

---
+++ Assets/Scripts/CannonBall.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBall : MonoBehaviour {
+    public Rigidbody2D rb;
+    [SerializeField] private float initialVelocity;
+
+    public void setInitialVelocity(float v, Vector2 direction) {
+        rb.velocity = direction * v;
+    }
+}
--- Assets/Scripts/CannonFire.cs
+++ Assets/Scripts/CannonFire.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CannonFire : MonoBehaviour {
     public GameObject cannonBall;
     public Animator anim;
+    [SerializeField] private float cannonBallSpeed = 5;
 
     private void Start() {
         GameEvents.fireCannons.AddListener(fire);
@@ -13,6 +14,8 @@ public class CannonFire : MonoBehaviour {
 
     private void fire() {
         anim.SetTrigger("fire");
-        Instantiate(cannonBall, transform.position, transform.rotation);
+        Vector3 direction = new Vector3(Mathf.Cos(transform.eulerAngles.z * Mathf.Deg2Rad), Mathf.Sin(transform.eulerAngles.z * Mathf.Deg2Rad), 0);
+        GameObject firedCannonBall = Instantiate(cannonBall, transform.position + direction, Quaternion.identity);
+        firedCannonBall.GetComponent<CannonBall>().setInitialVelocity(cannonBallSpeed, direction);
     }
 }
